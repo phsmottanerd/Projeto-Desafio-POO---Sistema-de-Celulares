f791ab12a57585a2712934c21cf76e4fcb2815fe
Paulo Henrique <jaspion235kit@gmail.com>
2025-06-17 14:10:24 -0300

    Projeto Desafio POO - Sistema de Celulares - Commit inicial

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Testando Nokia:");
+Smartphone nokia = new Nokia("9999-9999", "Nokia Tijolão", "IMEI12345", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("\nTestando iPhone:");
+Smartphone iphone = new Iphone("8888-8888", "iPhone 14", "IMEI67890", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
